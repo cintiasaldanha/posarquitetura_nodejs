87c43c6cbda8ea48e1b8305ab0f2234ade2da5f1
Cintia Saldanha <cintiasaldanha@gmail.com>
2020-12-10 23:34:15 -0300

    Ajustes Versão 1.0.3

--- AULA2/projeto/Atividade2v1/funcoes.js
+++ AULA2/projeto/Atividade2v1/funcoes.js
@@ -11,89 +11,92 @@ V 1.0.1:
 
 V 1.0.2
 - 10/12/2020: Correção module.exports
+
+V 1.0.2
+- 10/12/2020, 23h26: Correção module.exports 2
 */
 
 /*Implementação das funções que retornam o resultado da operação matemática 
 entre  os 2 números recebidos como entrada e o tipo de o operação informado*/
 
-function calcularResultadoPorOperacao(n1, n2, operator) {
-        try{
-            switch (String(operator)) {
-                //Adição
-                case "+":
-                case " ":
-                    return parseFloat(n1) + parseFloat(n2);
-                    break;
-                //Subtração
-                case "-":
-                    return parseFloat(n1) - parseFloat(n2);
-                    break;
-                //Multiplicação
-                case "*":
-                    return parseFloat(n1) * parseFloat(n2);
-                    break;
-                //Divisão      
-                case "/":
-                    if (parseInt(n2)==0) {
-                        return ">>>ATENÇÃO: O valor do divisor não pode ser igual a zero!"
-                    }
-                    else return parseFloat(n1) / parseFloat(n2);
-                    break;
-                default:
-                    return ">>>ATENÇÃO: Informe um operador válido: Adição(+), Subtração(-), Multiplicação(*) ou Divisão(/)."
-                    break;
+/*Implementação das funções que retornam o resultado da operação matemática 
+entre  os 2 números recebidos como entrada e o tipo de o operação informado*/
+function calcularResultadoPorOperacao(n1, n2, operador) {
+    try{
+        switch (String(operador)) {
+            //Adição
+            case "+":
+            case " ":
+                return parseFloat(n1) + parseFloat(n2);
+                break;
+            //Subtração
+            case "-":
+                return parseFloat(n1) - parseFloat(n2);
+                break;
+            //Multiplicação
+            case "*":
+                return parseFloat(n1) * parseFloat(n2);
+                break;
+            //Divisão      
+            case "/":
+                if (parseInt(n2)==0) {
+                    return ">>>ATENÇÃO: O valor do divisor não pode ser igual a zero!"
+                }
+                else return parseFloat(n1) / parseFloat(n2);
+                break;
+            default:
+                return ">>>ATENÇÃO: Informe um operador válido: Adição(+), Subtração(-), Multiplicação(*) ou Divisão(/)."
+                break;
             }
         }
         catch (ex) {
             return ex.message;
         }
+    };
     
-}
-
 function calcularAdicao(x, y) {
-        try{
-            return parseFloat(x) + parseFloat(y);
-        }
-        catch (ex) {
-            return ex.message;
-        }
-}
-    
-function calcularSubtracao(x, y) {
-        try{
-            return parseFloat(x) - parseFloat(y);
-        }
-        catch (ex) {
-            return ex.message;
-        }
+   try{
+        return parseFloat(x) + parseFloat(y);
     }
+    catch (ex) {
+        return ex.message;
+    }
+};
+
+function calcularSubtracao(x, y) {
+    try{
+        return parseFloat(x) - parseFloat(y);
+    }
+    catch (ex) {
+        return ex.message;
+    }
+};
 
 function calcularMultiplicacao(x, y) {
-        try{
-            return parseFloat(x) * parseFloat(y);
-        }
-        catch (ex) {
-            return ex.message;
-        }
+    try{
+       return parseFloat(x) * parseFloat(y);
     }
-    
+    catch (ex) {
+       return ex.message;
+    }
+};
+
 function calcularDivisao(x, y) {
-        try{
-            if (parseInt(y)==0) {
+    try{
+        if (parseInt(y)==0) {
                 return ">>>ATENÇÃO: O valor do divisor não pode ser igual a zero!"
-            }
+        }
             else return parseFloat(x) / parseFloat(y);
-        }
-        catch (ex) {
-            return ex.message;
-        }
     }
+    catch (ex) {
+       return ex.message;
+    }
+}
 
-module.exports.calcularResultadoPorOperacao = calcularResultadoPorOperacao;   
-module.exports.calcularAdicao = calcularAdicao;
-module.exports.calcularSubtracao = calcularSubtracao;
-module.exports.calcularMultiplicacao = calcularMultiplicacao;
-module.exports.calcularDivisao = calcularDivisao;
-
-
-
+module.exports = {
+    calcularResultadoPorOperacao: calcularResultadoPorOperacao,
+    calcularAdicao: calcularAdicao,
+    calcularSubtracao: calcularSubtracao,
+    calcularMultiplicacao: calcularMultiplicacao,
+    calcularDivisao: calcularDivisao
+};
